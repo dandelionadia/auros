ee3084c73c1be0c80955941d8d0f7bfb072d3435
dandelionadia <nadia.ridko@gmail.com>
2019-12-05 17:17:56 +0100

    adds colors

---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    grey: 'rgb(102, 102, 102)',
+    greyDim: '#f5f5f5',
+    greyLight: '#999',
+    black: '#000',
+    blackLight: '#222',
+    orange: '#eb7025',
+    orangeLight: '#ffb14b'
+  }
+}
