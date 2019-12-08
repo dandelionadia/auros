867bd2270bbb28c4e5bf9fe486b856b3a036ffca
dandelionadia <nadia.ridko@gmail.com>
2019-12-08 11:59:43 +0100

    adds heading

--- src/atoms/Heading.tsx
+++ src/atoms/Heading.tsx
@@ -1,5 +1,17 @@
 import styled, { css } from 'styled-components'
 
-const Heading = styled.h1``
+interface HeadingProps {
+  as?: string
+}
+
+const Heading = styled.h1<HeadingProps>`
+  font-size: 2.5rem; /* 36px */
+
+  ${props =>
+    props.as === `h2` &&
+    css`
+      font-size: 1.7rem; /* 24px */
+    `}
+`
 
 export { Heading }
