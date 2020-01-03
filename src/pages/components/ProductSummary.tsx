60e27b9939224cb7aa67aa94a599a43e62f87c8d
dandelionadia <nadia.ridko@gmail.com>
2020-01-03 10:43:18 +0100

    productSummary: adds styled description, subtitle, product rating

--- src/pages/components/ProductSummary.tsx
+++ src/pages/components/ProductSummary.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Composition } from 'atomic-layout'
+import { Box, Composition } from 'atomic-layout'
 import { Image } from '../../atoms/Image'
 import { Heading } from '../../atoms/Heading'
+import styled from 'styled-components'
 
 const templateMobile = `
 	gallery
@@ -11,6 +12,20 @@ const templateMobile = `
 const templateDesktop = `
 	gallery summary
 	/ 1fr 1fr
+`
+
+const StyledDescription = styled.p`
+  color: #999;
+  line-height: 28px;
+`
+
+const StyledProdactPrice = styled.span`
+  color: ${({ theme }) => theme.colors.greyLight};
+  font-size: 1.7rem; /* 24px */
+`
+
+const StyledP = styled.p`
+  margin: 0 0 40px;
 `
 
 const ProductSummary: React.FC = () => {
@@ -30,8 +45,30 @@ const ProductSummary: React.FC = () => {
             />
           </Gallery>
           <Summary>
-            <Heading>h1</Heading>
-            <Heading as="h2">h2</Heading>
+            <Heading>UNA Chair</Heading>
+            <Box flex marginBottom={1.1}>
+              <Box marginRight={1}>⋆ ✢ ✥ ✦ ✧ ❂ ❉ ✯</Box>
+              <a href="#">(5 customer reviews)</a>
+            </Box>
+            <StyledP>
+              <StyledProdactPrice>$102.84</StyledProdactPrice>
+            </StyledP>
+            <StyledDescription>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium in dolor quas maiores, non enim quos modi
+              necessitatibus cumque architecto quo odio adipisci repellendus
+              explicabo hic fugiat perferendis sunt. Facere.
+            </StyledDescription>
+            <StyledDescription>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium in dolor quas maiores, non enim quos modi
+              necessitatibus cumque architecto quo odio adipisci repellendus
+              explicabo hic fugiat perferendis sunt. Facere. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Ratione fuga qui rem
+              repellat earum beatae quas, sit possimus aliquid quasi deleniti
+              veritatis. Ratione aperiam quaerat veniam at placeat laudantium
+              non.
+            </StyledDescription>
           </Summary>
         </>
       )}
