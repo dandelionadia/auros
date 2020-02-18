e9a39a51f908d1ad9d8e178dcfc7fa1e9a9aeb64
dandelionadia <nadia.ridko@gmail.com>
2020-02-18 11:56:11 +0100

    Tabs: styled p on the lg

--- src/molecules/Tabs.tsx
+++ src/molecules/Tabs.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink, Switch, withRouter, Route } from 'react-router-dom'
 import styled from 'styled-components'
-import { Box } from 'atomic-layout'
+import Layout, { Box } from 'atomic-layout'
 
 const StyledLink = styled(NavLink)`
   color: ${({ theme }) => theme.colors.greyLight};
@@ -45,6 +45,13 @@ const StyledLI = styled.li`
   }
 `
 
+const StyledP = styled.p`
+  @media (min-width: ${Layout.breakpoints.lg.minWidth}) {
+    margin-left: 120px;
+    margin-right: 120px;
+  }
+`
+
 const Tabs = withRouter(({ match }) => {
   return (
     <Box marginBottom={3}>
@@ -66,34 +73,34 @@ const Tabs = withRouter(({ match }) => {
           path={match.url}
           exact
           render={() => (
-            <p>
+            <StyledP>
               I'm description Lorem ipsum dolor sit amet consectetur adipisicing
               elit. Quo sapiente tenetur cum, quis, quia veniam quod fugit
               eligendi cupiditate vero, sequi asperiores cumque eos ipsum
               pariatur alias dolore saepe unde.
-            </p>
+            </StyledP>
           )}
         />
         <Route
           path={`${match.url}/info`}
           render={() => (
-            <p>
+            <StyledP>
               I'm info Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Inventore dolorum similique officia ipsa magni, expedita
               consequuntur pariatur perferendis ipsam at reprehenderit,
               voluptate id, debitis amet facilis voluptatibus qui illo commodi.{' '}
-            </p>
+            </StyledP>
           )}
         />
         <Route
           path={`${match.url}/review`}
           render={() => (
-            <p>
+            <StyledP>
               I'm review Lorem ipsum dolor, sit amet consectetur adipisicing
               elit. Quia inventore accusamus in suscipit minus praesentium
               voluptate laboriosam exercitationem fugiat, perferendis illum. Et
               ad dolore beatae consequuntur soluta animi amet ullam.
-            </p>
+            </StyledP>
           )}
         />
       </Switch>
