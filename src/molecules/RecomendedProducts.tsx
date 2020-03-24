3fd5f8541f70dc97c050250c55cba28ce29e348e
dandelionadia <nadia.ridko@gmail.com>
2020-03-24 10:22:16 +0100

    RecomendedProducts: used QueryHook insted of fetch

--- src/molecules/RecomendedProducts.tsx
+++ src/molecules/RecomendedProducts.tsx
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { Composition } from 'atomic-layout'
 import { ProductItem } from './ProductItem'
 import { ProductData } from '../pages/product/ProductPage'
+import { useQuery } from '../hooks/useQuery'
 
 export const RecomendedProducts: React.FC<{
   productIds: string[]
 }> = ({ productIds }) => {
-  const [productsData, setProductsData] = useState<ProductData[]>()
+  const { loading, data } = useQuery<ProductData[]>(
+    `/products?id=${productIds.join()}`
+  )
 
-  useEffect(() => {
-    fetch(
-      `https://auros-api.netlify.com/.netlify/functions/api/products?id=${productIds.join()}`
-    )
-      .then(res => res.json())
-      .then(res => setProductsData(res))
-  }, [productIds])
+  if (loading) {
+    return <p>Products is loading...</p>
+  }
 
-  if (!productsData) {
-    return <p>loading...</p>
+  if (!data) {
+    return <p>Error when fetching product</p>
   }
 
   return (
@@ -27,7 +26,7 @@ export const RecomendedProducts: React.FC<{
       templateColsMd="repeat(2, 1fr)"
       templateColsLg="repeat(auto-fit, minmax(80px, 1fr))"
     >
-      {productsData.map(item => (
+      {data.map(item => (
         <ProductItem
           id={item.id}
           image="https://demo2.wpopal.com/auros/wp-content/uploads/2018/10/1-28-600x675.jpg"
