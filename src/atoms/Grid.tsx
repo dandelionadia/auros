30b10b4cb86660915e68d412d75daac977ac0cc1
dandelionadia <nadia.ridko@gmail.com>
2020-03-30 13:22:28 +0200

    Grid: changes margin horizontal; RecomendedProducts: change responsive props

--- src/atoms/Grid.tsx
+++ src/atoms/Grid.tsx
@@ -7,7 +7,7 @@ export const Grid: React.FC = ({ children }) => {
       paddingVertical={1.3}
       paddingVerticalMd={2.6}
       marginHorizontal={2.6}
-      marginHorizontalLg={6}
+      marginHorizontalLg={8}
     >
       {children}
     </Box>
