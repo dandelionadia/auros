57a8068e7dc000d6611d04e9de91b0a6662b8aee
dandelionadia <nadia.ridko@gmail.com>
2020-03-31 11:51:24 +0200

    CartPage: adds title bar

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Layout, { Composition, Box, Only } from 'atomic-layout'
 import styled from 'styled-components'
 import { IoMdClose } from 'react-icons/io'
 import { Heading } from '../../atoms/Heading'
 import { Grid } from '../../atoms/Grid'
+import { IoIosArrowForward } from 'react-icons/io'
 
 const templateTablet = `
 cart
@@ -56,6 +58,7 @@ const StyledImage = styled.img`
   max-width: 100px;
   height: auto;
   padding-left: 2rem;
+
   @media (min-width: ${Layout.breakpoints.lg.minWidth}) and (max-width: ${Layout
       .breakpoints.lg.maxWidth}) {
     width: 65px;
@@ -65,6 +68,10 @@ const StyledImage = styled.img`
 const StyledCartTotals = styled.div`
   background-color: #fafafa;
   padding: 40px;
+
+  @media (max-width: ${Layout.breakpoints.sm.maxWidth}) {
+    margin-bottom: 15px;
+  }
 `
 
 const StyledButton = styled.div`
@@ -97,92 +104,137 @@ const StyledProductTitles = styled.span`
   font-weight: bold;
 `
 
+const StyledTitleBar = styled.div`
+  background-color: '#fff';
+  background-image: url('https://demo2.wpopal.com/auros/wp-content/uploads/2018/10/breadcrumb.jpg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  width: 100%;
+  min-height: 140px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledTitle = styled.span`
+  color: #222;
+  font-size: 40px;
+  font-weight: bold;
+`
+
+const StyledLinkHome = styled.div`
+  color: #777;
+  font-size: 13px;
+  padding-right: 10px;
+`
+
+const StyledSpan = styled.span`
+  color: #000;
+  font-size: 13px;
+  padding-left: 10px;
+`
+
 export const CartPage: React.FC = () => {
   return (
-    <Grid>
-      <Composition template={templateTablet} templateLg={templateLg} gap={2}>
-        {Areas => (
-          <>
-            <Areas.Cart>
-              <Composition
-                as={StyledCart}
-                paddingVertical={2}
-                templateColsMd="1fr repeat(4,1fr)"
-                gap={2}
-                alignItems="center"
-                justifyItemsMd="center"
-              >
-                <Box
-                  flex
-                  flexDirection="row"
-                  alignItemsMd="center"
-                  justifyContentSmDown="center"
-                >
-                  <IoMdClose fill="#bbb" />
-                  <StyledImage src="https://demo2.wpopal.com/auros/wp-content/uploads/2018/10/1-1-600x675.jpg" />
-                </Box>
-                <Box flex flexDirection="row" justifyContent="space-between">
-                  <Only to="md">
-                    <StyledProductTitles>Product: </StyledProductTitles>
-                  </Only>
-                  <span>Janus Table Lamp </span>
-                </Box>
-                <StyledProductPrice>
-                  <Only to="md">
-                    <StyledProductTitles>Price: </StyledProductTitles>
-                  </Only>
-                  <span>$199</span>
-                </StyledProductPrice>
-                <StyledContainerInput>
-                  <Only to="md">
-                    <StyledProductTitles>Quantity: </StyledProductTitles>
-                  </Only>
-                  <StyledInput
-                    type="number"
-                    id="number"
-                    name="tentacles"
-                    min="1"
-                    max="10"
-                    defaultValue="1"
-                  />
-                </StyledContainerInput>
-                <StyledProductSubtotal>
-                  <Only to="md">
-                    <StyledProductTitles>Total: </StyledProductTitles>
-                  </Only>
-                  <span>$199.99</span>
-                </StyledProductSubtotal>
-              </Composition>
-            </Areas.Cart>
-            <Areas.CartTotals>
-              <Box as={StyledCartTotals} widthLg="200px" widthXl="250px">
-                <Heading as="h2">Cart totals</Heading>
+    <>
+      <StyledTitleBar>
+        <Box paddingVertical="20px" paddingHorizontal="40px">
+          <StyledTitle>Cart</StyledTitle>
+          <Box flex justifyContent="center" alignItems="center">
+            <StyledLinkHome as={Link} to="/">
+              Home
+            </StyledLinkHome>
+            <Box as={IoIosArrowForward} marginTop="2px" size="10px" />
+            <StyledSpan>Cart</StyledSpan>
+          </Box>
+        </Box>
+      </StyledTitleBar>
+      <Grid>
+        <Composition template={templateTablet} templateLg={templateLg} gap={2}>
+          {Areas => (
+            <>
+              <Areas.Cart>
                 <Composition
-                  templateCols="repeat(2,1fr)"
-                  paddingVertical="30px"
-                  as={StyledBoxPrice}
-                >
-                  <span>Subtotal:</span>
-                  <StyledProductSubtotal>$199.99</StyledProductSubtotal>
-                </Composition>
-                <Composition
-                  templateCols="repeat(2,1fr)"
-                  paddingVertical="30px"
+                  as={StyledCart}
+                  paddingVertical={2}
+                  templateColsMd="1fr repeat(4,1fr)"
+                  gap={2}
                   alignItems="center"
-                  flex
-                  justifyContent="space-between"
+                  justifyItemsMd="center"
                 >
-                  <span>Total:</span>
-                  <StyledTotalPrice>$199.99</StyledTotalPrice>
+                  <Box
+                    flex
+                    flexDirection="row"
+                    alignItemsMd="center"
+                    justifyContentSmDown="center"
+                  >
+                    <IoMdClose fill="#bbb" />
+                    <StyledImage src="https://demo2.wpopal.com/auros/wp-content/uploads/2018/10/1-1-600x675.jpg" />
+                  </Box>
+                  <Box flex flexDirection="row" justifyContent="space-between">
+                    <Only to="md">
+                      <StyledProductTitles>Product: </StyledProductTitles>
+                    </Only>
+                    <span>Janus Table Lamp </span>
+                  </Box>
+                  <StyledProductPrice>
+                    <Only to="md">
+                      <StyledProductTitles>Price: </StyledProductTitles>
+                    </Only>
+                    <span>$199</span>
+                  </StyledProductPrice>
+                  <StyledContainerInput>
+                    <Only to="md">
+                      <StyledProductTitles>Quantity: </StyledProductTitles>
+                    </Only>
+                    <StyledInput
+                      type="number"
+                      id="number"
+                      name="tentacles"
+                      min="1"
+                      max="10"
+                      defaultValue="1"
+                    />
+                  </StyledContainerInput>
+                  <StyledProductSubtotal>
+                    <Only to="md">
+                      <StyledProductTitles>Total: </StyledProductTitles>
+                    </Only>
+                    <span>$199.99</span>
+                  </StyledProductSubtotal>
                 </Composition>
-                <StyledButton>
-                  <StyledLink href="#">Proceed To Checkout</StyledLink>
-                </StyledButton>
-              </Box>
-            </Areas.CartTotals>
-          </>
-        )}
-      </Composition>
-    </Grid>
+              </Areas.Cart>
+              <Areas.CartTotals>
+                <Box as={StyledCartTotals} widthLg="200px" widthXl="250px">
+                  <Heading as="h2">Cart totals</Heading>
+                  <Composition
+                    templateCols="repeat(2,1fr)"
+                    paddingVertical="30px"
+                    as={StyledBoxPrice}
+                  >
+                    <span>Subtotal:</span>
+                    <StyledProductSubtotal>$199.99</StyledProductSubtotal>
+                  </Composition>
+                  <Composition
+                    templateCols="repeat(2,1fr)"
+                    paddingVertical="30px"
+                    alignItems="center"
+                    flex
+                    justifyContent="space-between"
+                  >
+                    <span>Total:</span>
+                    <StyledTotalPrice>$199.99</StyledTotalPrice>
+                  </Composition>
+                  <StyledButton>
+                    <StyledLink href="#">Proceed To Checkout</StyledLink>
+                  </StyledButton>
+                </Box>
+              </Areas.CartTotals>
+            </>
+          )}
+        </Composition>
+      </Grid>
+    </>
   )
 }
