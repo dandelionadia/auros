95cefbfc2fe0904b9e89dd89a9b37978a612054c
dandelionadia <nadia.ridko@gmail.com>
2020-04-21 19:27:26 +0200

    if index does not exist then add it, if it does then do not create a new one but rewrite a quantity

--- src/store/reducers/cart/cart.reducer.ts
+++ src/store/reducers/cart/cart.reducer.ts
@@ -1,8 +1,25 @@
-import { CartState, initialState } from './cart.state'
+import { CartState, initialState, CartItem } from './cart.state'
 import { ADD_TO_CART } from './types'
 
 export const cartReducer = (state: CartState = initialState, action: any) => {
   if (action.type === ADD_TO_CART) {
+    const stateItems = state.items
+
+    // ...try to find exist index if not going then go to the nextItems
+    const existingIndex = stateItems.findIndex((item) => {
+      return item.id === action.id
+    })
+
+    if (existingIndex >= 0) {
+      stateItems[existingIndex].quantity =
+        stateItems[existingIndex].quantity + action.quantity
+
+      return {
+        items: stateItems,
+      }
+    }
+
+    //adds new product
     const nextItems = state.items.concat({
       id: action.id,
       title: action.title,
