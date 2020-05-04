f9451d6b05bcd1d2059fdf5d097685460c5851c3
dandelionadia <nadia.ridko@gmail.com>
2020-05-04 11:52:09 +0200

    chage Grid component on the screens

--- src/atoms/Grid.tsx
+++ src/atoms/Grid.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Box } from 'atomic-layout'
+import styled from 'styled-components'
+
+const StyledBox = styled.div`
+  box-sizing: border-box;
+`
 
 export const Grid: React.FC = ({ children }) => {
   return (
     <Box
-      paddingVertical={1.3}
-      paddingVerticalMd={2.6}
-      marginHorizontal={2.6}
-      marginHorizontalLg={8}
+      as={StyledBox}
+      width="100%"
+      maxWidthSm="540px"
+      maxWidthMd="1200px"
+      marginHorizontal="auto"
+      paddingHorizontal={2.6}
+      paddingHorizontalLg={8}
     >
       {children}
     </Box>
