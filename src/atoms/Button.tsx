a302108e4ac135ce4770f79c03bd4b77f4d709b9
dandelionadia <nadia.ridko@gmail.com>
2020-05-06 16:12:57 +0200

    Button: fixes button size and pudding on the mobile screen

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
+import { query } from 'atomic-layout'
 
 export const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.black};
   color: ${({ theme }) => theme.colors.white};
   font-weight: 400;
-  font-size: 16px;
   text-transform: capitalize;
-  padding: 16px;
+  padding: 16px 10px;
   border: 0;
   width: 100%;
+
+  @media ${query({ from: 'sm' })} {
+    font-size: 16px;
+  }
 
   :hover {
     background-color: ${({ theme }) => theme.colors.orangeLight};
