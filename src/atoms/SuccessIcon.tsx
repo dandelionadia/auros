cfeb8cfd5874c330194fd1535c5b5f88156947ec
dandelionadia <nadia.ridko@gmail.com>
2020-05-07 15:06:52 +0200

    SuccessIcon: adds svg

---
+++ src/atoms/SuccessIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const SuccessIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="72px" height="72px">
+      <g fill="none" stroke="#8EC343" strokeWidth="2">
+        <path
+          className="path"
+          d="M17.417,37.778l9.93,9.909l25.444-25.393"
+          stroke="#000"
+          strokeWidth="4"
+        ></path>
+      </g>
+    </svg>
+  )
+}
