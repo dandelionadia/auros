f2cf3a3d18fa152eca7a06b3bd16f552addd40d7
dandelionadia <nadia.ridko@gmail.com>
2020-06-03 16:38:28 +0200

    Contactpage: adds responsive templates

--- src/pages/contactUs/ContactPage.tsx
+++ src/pages/contactUs/ContactPage.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Composition } from 'atomic-layout'
+
 import { TitleBar } from '../components/TitleBar'
 import { Grid } from '../../atoms/Grid'
 import { ContainerVertical } from '../../atoms/ContainerVertical'
+import { TitleBlock } from './components/TitleBlock'
+
+const templateMobile = `
+  titleInfo
+  contactInfo
+  titleForm
+  contactForm
+`
+
+const templateTablet = `
+  titleInfo contactInfo
+  titleForm contactForm
+  / auto 1fr
+`
 
 export const ContactPage: React.FC = () => {
   return (
@@ -9,7 +25,26 @@ export const ContactPage: React.FC = () => {
       <TitleBar titlePage="Contact Us" />
       <Grid fluid>
         <ContainerVertical>
-          <p>content</p>
+          <Composition
+            template={templateMobile}
+            templateMd={templateTablet}
+            gap={2}
+            gapMd={5}
+            gapLg={10}
+          >
+            {(Areas) => (
+              <>
+                <Areas.TitleInfo>
+                  <TitleBlock title="Infomation" />
+                </Areas.TitleInfo>
+                <Areas.ContactInfo>info</Areas.ContactInfo>
+                <Areas.TitleForm>
+                  <TitleBlock title="Drop us a line" />
+                </Areas.TitleForm>
+                <Areas.ContactForm>form</Areas.ContactForm>
+              </>
+            )}
+          </Composition>
         </ContainerVertical>
       </Grid>
     </>
