1a09174580d9e03dc25d967c47e8b174f3e13303
dandelionadia <nadia.ridko@gmail.com>
2020-06-12 20:12:50 +0200

    adds state and check onChange

--- src/pages/contactUs/components/Form.tsx
+++ src/pages/contactUs/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Composition, Box, query } from 'atomic-layout'
 
@@ -30,8 +30,8 @@ const StyledButton = styled(Button)`
   border: 2px solid ${({ theme }) => theme.colors.blackLight};
   font-size: 14px;
   margin: 30px 0;
-  width: auto;
   padding: 18px 28px;
+  max-width: max-content;
 
   :hover {
     background-color: ${({ theme }) => theme.colors.white};
@@ -51,39 +51,88 @@ const ContainerCheckbox = styled.div`
 `
 
 export const Form: React.FC = () => {
+  const [nameValue, setNameValue] = useState('')
+  const [emailValue, setEmailValue] = useState('')
+  const [messageValue, setMessageValue] = useState('')
+  const [subscribed, setSubscribed] = useState(false)
+
+  const handleNameChange = (event: any) => {
+    setNameValue(event.target.value)
+  }
+
+  const handleEmailChange = (event: any) => {
+    setEmailValue(event.target.value)
+  }
+
+  const handleMessageChange = (event: any) => {
+    setMessageValue(event.target.value)
+  }
+
+  const handleSubscribedChange = (event: any) => {
+    setSubscribed(event.target.checked)
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+
+    const values = {
+      name: nameValue,
+      email: emailValue,
+      message: messageValue,
+      subscribed,
+    }
+
+    console.warn(values)
+  }
+
   return (
-    <Composition gapXs={2} gapLg={5} marginLeftSmDown="20px">
-      <Box>
-        <Label htmlFor="name">Your Name:</Label>
-        <StyledInput
-          id="name"
-          type="name"
-          name="name"
-          placeholder="Ex: John Doe"
-          required
-        />
-      </Box>
-      <Box>
-        <Label htmlFor="email">E-Mail:</Label>
-        <StyledInput
-          id="email"
-          type="email"
-          name="email"
-          placeholder="Ex: John_Doe@domain.com"
-          required
-        />
-      </Box>
-      <Box>
-        <Label htmlFor="story">Message</Label>
-        <StyledTextarea id="story" name="story">
-          Enter text here...
-        </StyledTextarea>
-      </Box>
-      <Box as={ContainerCheckbox}>
-        <input type="checkbox" id="story" />
-        <StyledSubscribe id="story">Also Subscribe Us</StyledSubscribe>
-      </Box>
-      <StyledButton id="story">Send Message</StyledButton>
-    </Composition>
+    <form onSubmit={handleSubmit}>
+      <Composition gapXs={2} gapLg={5} marginLeftSmDown="20px">
+        <Box>
+          <Label htmlFor="name">Your Name:</Label>
+          <StyledInput
+            id="name"
+            type="name"
+            name="name"
+            placeholder="Ex: John Doe"
+            value={nameValue}
+            onChange={handleNameChange}
+            required
+          />
+        </Box>
+        <Box>
+          <Label htmlFor="email">E-Mail:</Label>
+          <StyledInput
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Ex: John_Doe@domain.com"
+            value={emailValue}
+            onChange={handleEmailChange}
+            required
+          />
+        </Box>
+        <Box>
+          <Label htmlFor="message">Message</Label>
+          <StyledTextarea
+            id="message"
+            name="message"
+            placeholder="Enter text here..."
+            value={messageValue}
+            onChange={handleMessageChange}
+          />
+        </Box>
+        <Box as={ContainerCheckbox}>
+          <input
+            type="checkbox"
+            name="subscribe"
+            checked={subscribed}
+            onChange={handleSubscribedChange}
+          />
+          <StyledSubscribe>Also Subscribe Us</StyledSubscribe>
+        </Box>
+        <StyledButton>Send Message</StyledButton>
+      </Composition>
+    </form>
   )
 }
